d20fcba3474a5fd8a7ad8dd2a7e28113575e56ad
James VB <james@myquidd.com>
2018-04-22 14:51:23 -0400

    initial

---
+++ BevtechCore/BevtechCore.h
@@ -0,0 +1,19 @@
+//
+//  BevtechCore.h
+//  BevtechCore
+//
+//  Created by James VanBeverhoudt on 4/22/18.
+//  Copyright © 2018 James VanBeverhoudt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BevtechCore.
+FOUNDATION_EXPORT double BevtechCoreVersionNumber;
+
+//! Project version string for BevtechCore.
+FOUNDATION_EXPORT const unsigned char BevtechCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BevtechCore/PublicHeader.h>
+
+
